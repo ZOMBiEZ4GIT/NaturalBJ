3a4e9f1cd0dbf679c4a43b25dc1a5ddea6ae471c
RolandGeorge <154392091+RolandGeorge@users.noreply.github.com>
2025-11-23 17:42:41 +1100

    Initial Commit

---
+++ NaturalBJTests/NaturalBJTests.swift
@@ -0,0 +1,17 @@
+//
+//  NaturalBJTests.swift
+//  NaturalBJTests
+//
+//  Created by Roland on 23/11/2025.
+//
+
+import Testing
+@testable import NaturalBJ
+
+struct NaturalBJTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
